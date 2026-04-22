f518c56e05034ea8a6407718502adc5f5e9bf3db
superdrive2000 <mgquinga@gmail.com>
2026-04-22 18:58:56 -0500

    Status: Modelo funcional:con inferencia local + UI +flask

--- inference/app.py
+++ inference/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,  render_template
 import joblib
 import whisper
 from prometheus_client import Counter, start_http_server
@@ -12,7 +12,7 @@ REQUESTS = Counter('request_count', 'Total requests')
 model, vectorizer = joblib.load("model/model.pkl")
 
 # modelo Whisper
-whisper_model = whisper.load_model("base")
+whisper_model = whisper.load_model("small")
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
@@ -33,6 +33,35 @@ def analyze():
         "text": text,
         "sentiment": int(pred)
     })
+    
+    
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/analyze-ui", methods=["POST"])
+def analyze_ui():
+    REQUESTS.inc()
+
+    file = request.files["audio"]
+    file.save("temp.wav")
+
+    # transcripción
+    result = whisper_model.transcribe("temp.wav")
+    text = result["text"]
+
+    # predicción
+    vec = vectorizer.transform([text])
+    pred = model.predict(vec)[0]
+
+    sentiment_label = "positivo" if pred == 1 else "negativo"
+
+    return render_template(
+        "index.html",
+        text=text,
+        sentiment=sentiment_label
+    )
+
 
 if __name__ == "__main__":
     start_http_server(8000)  # Prometheus
